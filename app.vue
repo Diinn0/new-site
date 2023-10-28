b1adfa529d6a509afc253fc514963a6bedab9f4b
Dino <dinocraftdidi@gmail.com>
2023-10-28 11:47:46 +0200

    chore: set main page
    Setup flowbite for all page

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+    <h1>Hello World!</h1>
+
+  </NuxtLayout>
 </template>
