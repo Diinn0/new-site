f48242e61fbaf48e4e1ee5cf6d89f5882c65b974
Dino <dinocraftdidi@gmail.com>
2023-10-28 11:49:07 +0200

    wip

---
+++ pages/cat/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useFirestore } from 'vuefire'
+import {doc} from "@firebase/firestore";
+import {collection} from "firebase/firestore";
+const db = useFirestore()
+const route = useRoute()
+
+
+const cat = doc(db, 'chat', route.params.id.toString())
+console.log(cat)
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
