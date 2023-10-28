05aeb8d0fc2eb52fa1adbce3b3602387d1466a31
Dino <dinocraftdidi@gmail.com>
2023-10-28 11:35:22 +0200

    chore: add tailwind and flowbite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-vuefire', "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  vuefire: {
+    config: {
+      apiKey: process.env.APIKEY,
+      authDomain: process.env.AUTHDOMAIN,
+      projectId: process.env.PROJECTID,
+      appId: process.env.APPID,
+      // there could be other properties depending on the project
+    },
+    appCheck: {
+      // Allows you to use a debug token in development
+      debug: process.env.NODE_ENV !== 'production',
+      isTokenAutoRefreshEnabled: true,
+      provider: 'ReCaptchaV3',
+      // Find the instructions in the Firebase documentation, link above
+      key: process.env.RECAPTCHAKEY,
+    },
+  },
 })
