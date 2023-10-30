f9b0643478a34b1640ba2a9cbb22c37d1ffa0bcb
Dino <dinocraftdidi@gmail.com>
2023-10-30 14:33:58 +0100

    feat: create cat page

--- pages/cat/[id].vue
+++ pages/cat/[id].vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
-import { useFirestore } from 'vuefire'
-import {doc} from "@firebase/firestore";
-import {collection} from "firebase/firestore";
-const db = useFirestore()
-const route = useRoute()
+import {doc, getDoc, getFirestore} from "@firebase/firestore/lite";
+import { useDocument } from 'vuefire'
 
+const route = useRoute();
+const db = getFirestore()
 
-const cat = doc(db, 'chat', route.params.id.toString())
-console.log(cat)
+const cat = (await getDoc(doc(db, 'chat', route.params.id.toString()))).data()
+
+if (cat == undefined)
+{
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+    data: {
+      myCustomField: true
+    }
+  })
+}
 
 </script>
 
 <template>
-
+  <section class="bg-white dark:bg-gray-900">
+    <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+      <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image">
+      <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image">
+      <div class="mt-4 md:mt-0">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{{ cat.name }}</h2>
+        <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
