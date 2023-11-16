6cbe753648fb4dfaf22875c8b7633a22a9ac3522
Dino <dinocraftdidi@gmail.com>
2023-11-16 09:32:15 +0100

    feat: some upgrade

--- app.vue
+++ app.vue
@@ -1,23 +1,10 @@
 <script setup>
 import 'flowbite';
 
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
-const router = useRouter()
-const route = useRoute()
-const user = useCurrentUser()
-
-// initialize components based on data attribute selectors
-onMounted(() => {
-  initFlowbite();
-})
-
 </script>
 
 <template>
   <NuxtLayout>
     <NuxtPage/>
-    <h1>Hello World!</h1>
-
   </NuxtLayout>
 </template>
--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import {useFirestore} from "vuefire";
 import {doc, getDoc} from "firebase/firestore";
+import {initFlowbite} from "flowbite";
+
+onMounted( () => {
+  initFlowbite();
+})
 
 const colorMode = useColorMode()
 const user = useCurrentUser()
@@ -67,10 +72,10 @@ const isDark = computed({
             </svg>
             <span class="sr-only">Toggle sidebar</span>
           </button>
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <NuxtLink href="/" class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Maison Fleurie</NuxtLink>
         </div>
         <div class="flex items-center lg:order-2">
-          
+
           <UToggle
               off-icon="i-heroicons-sun-20-solid"
               on-icon="i-heroicons-moon-20-solid"
@@ -733,8 +738,8 @@ const isDark = computed({
         </form>
         <ul class="space-y-2">
           <li>
-            <a
-                href="#"
+            <NuxtLink
+                href="/admin"
                 class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg
@@ -748,68 +753,25 @@ const isDark = computed({
                 <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
               </svg>
               <span class="ml-3">Overview</span>
-            </a>
+            </NuxtLink>
           </li>
           <li>
-            <button
+            <NuxtLink href="/admin/cats"
                 type="button"
-                class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                aria-controls="dropdown-pages"
-                data-collapse-toggle="dropdown-pages"
-            >
+                class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
               <svg
                   aria-hidden="true"
                   class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor"
                   viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
+                  xmlns="http://www.w3.org/2000/svg">
                 <path
                     fill-rule="evenodd"
                     d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                    clip-rule="evenodd"
-                ></path>
+                    clip-rule="evenodd"></path>
               </svg>
-              <span class="flex-1 ml-3 text-left whitespace-nowrap"
-              >Chat</span
-              >
-              <svg
-                  aria-hidden="true"
-                  class="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <ul id="dropdown-pages" class="hidden py-2 space-y-2">
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Settings</a
-                >
-              </li>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Kanban</a
-                >
-              </li>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Calendar</a
-                >
-              </li>
-            </ul>
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">Chat</span>
+            </NuxtLink>
           </li>
           <li>
             <button
