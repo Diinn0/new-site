4323dd1dcbadffe350d06b4b2cdd194051154bbe
Maël Roy <dinocraftdidi@gmail.com>
2023-11-17 17:41:00 +0100

    feat: improve lisibility

--- pages/admin/cats/index.vue
+++ pages/admin/cats/index.vue
@@ -29,23 +29,23 @@
             <tbody>
             <tr class="border-b dark:border-gray-700" v-for="cat in cats">
               <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ cat.name }}</th>
-              <td class="px-4 py-3">{{ cat.sexe == 0 ? "Femelle" : "Male" }}</td>
-              <td class="px-4 py-3">{{ cat.repro ? "Non neutré" : "Neutré" }}</td>
-              <td class="px-4 py-3">{{ cat.active ? "Oui" : "Non" }}</td>
+              <td :class="'px-4 py-3 ' + (cat.sexe == 0 ? 'text-rose-400' : 'text-blue-600')">{{ cat.sexe == 0 ? "Femelle" : "Male" }}</td>
+              <td :class="'px-4 py-3 ' + (cat.neutre ? 'text-red-600' : 'text-green-600')">{{ !cat.neutre ? "Non neutré" : "Neutré" }}</td>
+              <td :class="'px-4 py-3 ' + (!cat.active ? 'text-red-600' : 'text-green-600')">{{ cat.active ? "Oui" : "Non" }}</td>
               <td class="px-4 py-3 flex items-center justify-end">
-                <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+                <button :data-dropdown-toggle="cat.id" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                   <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                   </svg>
                 </button>
-                <div id="apple-imac-27-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                <div :id="cat.id" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                   <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
                     <li>
-                      <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Modifier</a>
+                      <NuxtLink :href="'/admin/cats/' + cat.id" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Modifier</NuxtLink>
                     </li>
                   </ul>
                   <div class="py-1">
-                    <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Supprimer</a>
+                    <a @click="preProcessDel(cat.id, cat.name)" href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Supprimer</a>
                   </div>
                 </div>
               </td>
@@ -96,14 +96,37 @@
         </nav>
       </div>
     </div>
+
+
+    <UModal v-model="isConfirmDelOpen">
+          <!-- Modal content -->
+          <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+            <button @click="isConfirmDelOpen = false" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal">
+              <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              <span class="sr-only">Fermer</span>
+            </button>
+            <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+            <p class="mb-4 text-gray-500 dark:text-gray-300">Etes-vous sur de vouloir supprimer le chat {{ delCatName }} ?</p>
+            <div class="flex justify-center items-center space-x-4">
+              <button @click="cancelDel()" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                Non, annuler
+              </button>
+              <button type="submit" @click="processDel()" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                Oui je suis sûr
+              </button>
+            </div>
+          </div>
+    </UModal>
+
   </section>
 
 </template>
 
 <script setup>
 import {useFirestore} from "vuefire";
-import {collection} from "firebase/firestore";
+import {collection, doc} from "firebase/firestore";
 import {initFlowbite} from "flowbite";
+import {deleteDoc} from "@firebase/firestore";
 
 onMounted( () => {
   initFlowbite();
@@ -114,10 +137,40 @@ definePageMeta({
   middleware: ['admin'],
 })
 
+const toast = useToast()
 const db = useFirestore()
-
 const cats = useDocument(collection(db, 'chat'))
 
+let isConfirmDelOpen = ref(false)
+let delCatName = '';
+let delCatId = '';
+let preProcessDel = (id, name) => {
+   delCatName = name;
+   delCatId = id;
+   isConfirmDelOpen = ref(true);
+}
+
+let cancelDel = () => {
+  delCatName = '';
+  delCatId = '';
+  isConfirmDelOpen = ref(false);
+}
+
+let processDel = () => {
+  isConfirmDelOpen = ref(false);
+  let chatColl = collection(db, 'chat');
+  deleteDoc(doc(db, 'chat', delCatId))
+      .then((result) => {
+        toast.add( {
+          title: 'Chat supprimé',
+          description: `${delCatName} à bien été supprimé`,
+          timeout: 6000,
+          color:'primary'
+        })
+      }).catch((error) => {
+        console.log(error)
+    });
+}
 </script>
 
 <style scoped>
