70377da0097a262ae300f0f4266ec5c0e4482bf9
Dino <dinocraftdidi@gmail.com>
2023-11-21 08:09:50 +0100

    feat: replace dots by buttons

--- pages/admin/cats/index.vue
+++ pages/admin/cats/index.vue
@@ -33,21 +33,15 @@
               <td :class="'px-4 py-3 ' + (cat.neutre ? 'text-red-600' : 'text-green-600')">{{ !cat.neutre ? "Non neutré" : "Neutré" }}</td>
               <td :class="'px-4 py-3 ' + (!cat.active ? 'text-red-600' : 'text-green-600')">{{ cat.active ? "Oui" : "Non" }}</td>
               <td class="px-4 py-3 flex items-center justify-end">
-                <button :data-dropdown-toggle="cat.id" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
-                  <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                </button>
-                <div :id="cat.id" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                  <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
-                    <li>
-                      <NuxtLink :href="'/admin/cats/' + cat.id" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Modifier</NuxtLink>
-                    </li>
-                  </ul>
-                  <div class="py-1">
-                    <a @click="preProcessDel(cat.id, cat.name)" href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Supprimer</a>
-                  </div>
-                </div>
+
+                <NuxtLink :href="'/admin/cats/' + cat.id" class="text-white bg-orange-400 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-orange-400 dark:hover:bg-orange-600 dark:focus:ring-orange-600">
+                  <Icon name="mdi:pencil"></Icon>
+                  <span class="sr-only">Editer</span>
+                </NuxtLink>
+                <a @click="preProcessDel(cat.id, cat.name)" href="#" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                  <Icon name="mdi:delete"></Icon>
+                  <span class="sr-only">Supprimer</span>
+                </a>
               </td>
             </tr>
             </tbody>
