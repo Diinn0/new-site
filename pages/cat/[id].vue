ac20568a3a53f0350b263bf0bb902e571d442da2
Maël Roy <dinocraftdidi@gmail.com>
2023-11-26 17:30:43 +0100

    feat: add list nav on cat page

--- pages/cat/[id].vue
+++ pages/cat/[id].vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import { useFirestore } from 'vuefire'
 import {doc, getDoc} from "firebase/firestore";
 import { useDocument } from 'vuefire'
@@ -19,15 +19,24 @@ if (cat == undefined)
   })
 }
 
+const links = [
+  { name: 'Nos chats', url: '/cat/list' },
+  { name: cat.name }
+]
+
 const tests = cat.tests;
+
+let currentId = undefined;
 
 </script>
 
 <template>
-  <section class="bg-white dark:bg-gray-900">
+  <section>
+    <InPageNav :links="links" />
     <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-      <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image">
-      <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image">
+      <div>
+
+      </div>
       <div class="mt-4 md:mt-0">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{{ cat.name }}</h2>
 
