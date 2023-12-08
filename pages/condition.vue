dd942db42941d9b17f740463525e7264fdb8a7b6
Dino <dinocraftdidi@gmail.com>
2023-12-08 14:41:46 +0100

    feat: add some pages
    Condition de vente + information about cats

---
+++ pages/condition.vue
@@ -0,0 +1,152 @@
+<script setup>
+const links = [
+  {name: 'Conditions de vente'}
+]
+</script>
+
+<template>
+  <InPageNav :links="links"/>
+  <div class="mx-5">
+    <h2 class="text-4xl font-extrabold dark:text-white mb-2 mt-3">CONDITIONS GENERALES DE RESERVATION ET DE VENTE </h2>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">PREAMBULE</h3>
+    <p class="mb-3">
+      Nous sommes un petit élevage familial à NAMPTEUIL SOUS MURET (02), la Chatterie de la
+      Maison Fleurie, représentée par Madame Karine Roy, Entrepreneur Individuel, immatriculé au
+      SIREN sous le numéro 538 940 057, Livre officiel des affixes 18314, Certificat d'Etudes
+      Techniques de l'animal de compagnie C-1671et Certificat de capacité délivré le 11/09/2012,
+      (Ci-après « le Vendeur »).
+    </p>
+    <p class="mb-3">
+      Nos chatons vivent avec nous à la maison et sont chouchoutés. Nous élevons nos chatons avec
+      beaucoup d’amour et sommes attentifs au profil des futurs propriétaires.
+    </p>
+    <p class="mb-3">
+      Les présentes conditions générales visent à définir les obligations réciproques des parties ainsi
+      que les différentes étapes du processus d’achat d’un chaton destiné exclusivement à la
+      compagnie. Elles précisent et complètent utilement les conditions générales de vente qui
+      figurent au dos du contrat de vente d’un chaton.
+    </p>
+    <p class="mb-3">
+      Toute réservation et achat d’un chaton implique l’acceptation totale et sans réserve des
+      présentes conditions par l’adoptant.
+    </p>
+    <p class="mb-3">
+      Le Vendeur se réserve la possibilité de modifier ces conditions générales de vente à tout
+      moment sans préavis: les conditions applicables seront celles en vigueur à la date de la
+      réservation d’un chaton par l’adoptant.
+    </p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">1. RESPECT ET BIEN ETRE DE L’ANIMAL
+    </h3>
+    <p class="mb-3">
+      Conformément à l’article L214-1 du Code rural, tout animal étant un être sensible doit être
+      placé par son propriétaire dans des conditions compatibles avec les impératifs biologiques de
+      son espèce.
+    </p>
+    <p class="mb-3">
+      Par ailleurs, il est rappelé que conformément à l’article L214-3 du Code rural, il est interdit
+      d'exercer des mauvais traitements envers les animaux domestiques.
+    </p>
+    <p class="mb-3">
+      Enfin, il est rappelé que conformément à l’article 521-1 du Code pénal, le fait, publiquement
+      ou non, d'exercer des sévices graves ou de commettre un acte de cruauté envers un animal
+      domestique, ou apprivoisé, ou tenu en captivité, est puni de trois ans d'emprisonnement et
+      de 45 000 euros d'amende.
+    </p>
+    <p class="mb-3">
+      En considération de ce qui précède, le Vendeur se réserve de sélectionner les adoptants.
+    </p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">2. DISPONIBILITE D’UN CHATON</h3>
+    <p class="mb-3">
+      Les chatons sont disponibles dès que ceux-ci sont sevrés, c’est-à-dire vers l’âge de 15 à 17
+      semaines.
+    </p>
+    <p class="mb-3">
+      L’adoptant devra venir à la chatterie pour rencontrer le chaton, dès la 4
+      ème semaine, après avoir convenu d’un rendez-vous avec le Vendeur.
+    </p>
+    <p class="mb-3">
+      A l’issue de ce rendez-vous, l’adoptant pourra, s’il le souhaite, poser une option, pendant une
+      durée maximale de 15 jours ou bien le réserver.
+    </p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">3. CONDITIONS DE RESERVATION</h3>
+    <p class="mb-3">
+      Un contrat de réservation est conclu entre le Vendeur et l’adoptant après une visite à la
+      chatterie. Aucune réservation d’un chaton n’est possible sans une visite préalable à la
+      chatterie.
+    </p>
+    <p class="mb-3">
+      Un acompte déterminé par le Vendeur est versé par l’adoptant au moment de la réservation
+      d’un chaton.
+    </p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">4. DELAIS</h3>
+    <p class="mb-3">
+      L’adoptant s’engage à acquérir le chaton réservé dans le délai qui sera convenu avec le
+      Vendeur.
+    </p>
+    <p class="mb-3">
+      A défaut de venir récupérer le chaton dans le délai convenu, l’adoptant sera considéré comme
+      ayant renoncé à l’achat du chaton lequel sera de nouveau disponible à la vente.
+    </p>
+    <p class="mb-3">
+      L’acompte versé restera acquis au Vendeur.
+    </p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">5. RETRAIT DU CHATON</h3>
+    <p class="mb-3">
+      Le chaton doit être retiré à la chatterie par l’adoptant le jour convenu avec le Vendeur.
+    </p>
+    <p class="mb-3">
+      Le Vendeur se réserve le droit de refuser la remise du chaton pour des raisons motivées et
+      dans l’intérêt du chaton.
+    </p>
+    <p class="mb-3">
+      Lors du retrait du chaton, le Vendeur remet à l’adoptant :</p>
+    <ul class="space-y-1 list-disc list-inside mb-2">
+      <li>Le carnet de santé du chaton,</li>
+      <li>Un certificat de bonne santé établi par le vétérinaire référent du Vendeur,</li>
+      <li>La copie des tests ADN et échographie des parents,</li>
+      <li>Le pédigrée LOOF ou le numéro provisoire,</li>
+      <li>Le contrat de vente</li>
+      <li>Un kit chaton.</li>
+    </ul>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">6. PRIX</h3>
+    <p class="mb-3">Le prix d’un chaton destiné à la compagnie est de 1400 euros.</p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">7. MODALITES DE PAIEMENT DU PRIX</h3>
+    <p class="mb-3">L’intégralité du prix doit être payé avant le retrait du chaton au Vendeur par virement
+      bancaire.</p>
+    <p class="mb-3">A défaut de règlement de l’intégralité du prix, l’adoptant sera considéré comme ayant renoncé
+      à l’achat du chaton.</p>
+    <p class="mb-3">Le chaton reste la propriété du Vendeur jusqu’au paiement intégral du prix.</p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">8. ENGAGEMENTS DU VENDEUR</h3>
+    <p class="mb-3">Avant son adoption, le Vendeur s’engage à :</p>
+    <ul class="space-y-1 list-disc list-inside mb-2">
+      <li>Faire vacciner le chaton contre le Typhus, le Coryza et la Leucose,</li>
+      <li>Faire stériliser le chaton,</li>
+      <li>Vermifuger le chaton,</li>
+      <li>Faire identifier le chaton par puce électronique.</li>
+    </ul>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">9. ENGAGEMENTS DE L’ADOPTANT</h3>
+    <p class="mb-3">L’adoptant s’engage à prendre rendez-vous auprès d’un vétérinaire de son choix pour assurer
+      une première visite au chaton nouvellement adopté.</p>
+    <p class="mb-3">L’adoptant s’engage à poursuivre les rappels de vaccination du chaton ainsi que le traitement
+      de vermifuge régulièrement conformément aux recommandations du Vendeur.</p>
+    <p class="mb-3">L’adoptant s’engage à accorder au chaton tous les soins indispensables à sa bonne santé et à
+      son équilibre.</p>
+    <p class="mb-3">L’adoptant s’interdit de revendre ou de donner à qui que ce soit le chaton adopté sans en
+      informer au préalable le Vendeur.</p>
+    <p class="mb-3">L’adoptant s’engage à donner au Vendeur des nouvelles du chaton adopté et de lui envoyer
+      des photos régulièrement.</p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">10. FORCE MAJEURE</h3>
+    <p class="mb-3">L’exécution des obligations du Vendeur peut être suspendue en cas de survenance d’un cas
+      de force majeure. Le Vendeur en avisera l’adoptant dès que possible et par tout moyen.</p>
+    <h3 class="text-lg font-extrabold dark:text-white mb-2">11. LITIGES</h3>
+    <p class="mb-3">Les contestations et différends, de quelque nature qu’ils soient, qui pourraient naître à
+      l’occasion de l’adoption d’un chaton seront, préalablement à toute action juridictionnelle au
+      fond ou en référé soumis à la médiation. A défaut d’accord trouvé en médiation, les Parties
+      retrouveront toute liberté afin d’user des voies de droit qui leur sont ouvertes. Le différend
+      sera soumis à la juridiction du Tribunal Judiciaire de Soissons.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/information.vue
@@ -0,0 +1,48 @@
+<script setup>
+const links = [
+  { name: 'Informations importante' }
+]
+</script>
+
+<template>
+  <InPageNav :links="links" />
+
+  <div class="items-center mx-auto max-w-screen-xl md:grid md:grid-cols-2 lg:px-6 align-top pt-4">
+    <div>
+      <h2 class="mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-4xl dark:text-white">Vente chatons</h2>
+      <h2 class="mb-2 mt-4 text-lg font-semibold dark:text-white">Nos chatons quittent l’élevage :</h2>
+      <ul class="max-w-md space-y-1 list-disc list-inside dark:text-white">
+        <li>A partir de 3 mois au minimum</li>
+        <li>Avec un carnet de santé</li>
+        <li>Vaccinés</li>
+        <li>Vermifugés</li>
+        <li>Identifiés par puce électronique</li>
+        <li>Avec un certificat de bonne santé établi par le vétérinaire</li>
+        <li>Avec la copie des tests ADN et échographique des parents</li>
+        <li>Le pédigrée LOOF ou le numéro provisoire</li>
+        <li>Un contrat de vente</li>
+        <li>Un kit chaton</li>
+      </ul>
+    </div>
+    <div >
+      <h2 class="mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-4xl dark:text-white">Quelques précautions</h2>
+      <p class="my-4 dark:text-white">Vous êtes décidés, vous allez acquérir le chaton de vos rêves.<br>
+        Il est utile de bien vous renseigner et de prendre quelques précautions.<br>
+        Internet est un outil efficace, mais attention car on y trouve le meilleur comme le pire:</p>
+
+      <ul class="space-y-1 list-disc list-inside dark:text-white">
+        <li>Attention aux prix bas, l’élevage de chats de race est une activité qui coûte cher quand elle est pratiquée correctement.</li>
+        <li>Assurez-vous que le chaton a bien un pedigree LOOF.</li>
+        <li>N’hésitez pas à poser des questions par mail ou par téléphone.</li>
+        <li>N’hésitez pas à vous rendre chez l’éleveur afin de vous rendre compte de la qualité de vie des chats.</li>
+        <li>Demandez les tests ADN et les échographies pour la HCM des parents.</li>
+        <li>Ne récupérez pas votre chaton avant qu’il ait 3 mois, vacciné et muni d’un certificat de santé établi dans les 5 jours précédent l’arrivé du chaton.</li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
